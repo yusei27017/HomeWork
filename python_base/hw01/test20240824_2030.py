f2b558361b094b7efb34fbec5ca6a0cbfc595ebd
063 <a055r00650@gmail.com>
2024-08-25 19:59:54 +0800

    add initial tset20240824_2030.py file

--- python_base/hw01/test20240824_2030.py
+++ python_base/hw01/test20240824_2030.py
@@ -1,37 +1,44 @@
 import json
 
-#file_path = 'data.json'
-file_path = './data.json'
-#file_path = 'd:/my_project/HomeWork/python_base/hw01/data.json'
+import os
+print(os.getcwd()) #確認當前資料夾位置
 
-# import os
-# print(os.getcwd())
+file_path = './data.json'
 
 def read_data():
     # 打開檔案並讀取內容
     with open(file_path, 'r', encoding='utf-8') as file:
+
         file_content = file.read()
+
         file_content = file_content.replace('\xa0', ' ')
+
         data = json.loads(file_content)
     # 現在 data 變數中保存著 JSON 檔案的資料，可以直接操作
+
+    if file.closed:
+	    print("file is closed")
+    else:
+	    print("file is still not closed")
+
     return data
 
+
+
 def data_replace(vehicles_data):
+
     print(vehicles_data)
-    '''
-    請完成這個function !
-    將我剛剛爬下來的gta線上模式的載具品牌跟名稱進行分類使用字典dict
-    key為品牌名稱 value為list載具名稱
-    例如{
-        '浪子':['柒-70', 'JB 700W', ..],
-        '古羅帝':['義塔力 GTO', '披治崇敬'..]
-        }
-    return dict給輸出
-    '''
+    #....
+
     pass
 
 if __name__ == "__main__":
+
     input_data = read_data()
+
     vehicles_data = input_data['vehicles']
+
     data_dict = data_replace(vehicles_data)
+
     print(data_dict)
+
