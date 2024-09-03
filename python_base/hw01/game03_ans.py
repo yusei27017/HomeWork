111af75af3a991a2b5596cc766670afa993e5869
yusei27017 <no101shark@gmail.com>
2024-09-03 12:14:19 +0800

    add game03_ans

--- python_base/hw01/game03_ans.py
+++ python_base/hw01/game03_ans.py
@@ -25,7 +25,12 @@ print
 *
 '''
 def print_delta(height):
-
+    delta_array = ['*'*i for i in range(1, height+1)]
+    for e in delta_array:
+        print(e)
+    delta_array.pop()
+    for r_e in delta_array[::-1]:
+        print(r_e)
     return
 
 
